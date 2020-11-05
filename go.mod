77176bec178bc89f603ff7bc36e0e472876db938
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-11-05 23:43:12 +0530

    Add migrations

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-pg/migrations/v8 v8.0.1
+	github.com/go-pg/pg/v10 v10.6.1
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.1.3
 	github.com/gofiber/jwt/v2 v2.0.1
---
+++ migrations/1_init.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-pg/migrations/v8"
+)
+
+func init() {
+	migrations.MustRegisterTx(func(db migrations.DB) error {
+		fmt.Println("creating table my_table...")
+		_, err := db.Exec(`CREATE TABLE my_table()`)
+		return err
+	}, func(db migrations.DB) error {
+		fmt.Println("dropping table my_table...")
+		_, err := db.Exec(`DROP TABLE my_table`)
+		return err
+	})
+}
